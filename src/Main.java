eaef7443305b3cffa2ddda3495b443a519b707ab
sesseljabs <sesseljabjarney@gmail.com>
2021-10-26 11:11:52 +0200

    even better

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Fun lol = new Fun();
         System.out.println(lol.cum);
+        System.out.println("kinda funny ngl");
     }
 }
